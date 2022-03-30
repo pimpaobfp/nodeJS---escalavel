9c71f2931ced6de78d74d5d60441fc527424c2bd
pimpaobfp <brunobothp@gmail.com>
2022-03-30 19:05:18 -0300

    Aula 5

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,3 +1,4 @@
+const { application } = require('express')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
 
 class Serializador {
@@ -7,9 +8,54 @@ class Serializador {
 
     serializar (dados) {
         if (this.contentType === 'application/json') {
-            return this.json(dados)
+            return this.json(
+                this.filtrar(dados)
+            )
         }
 
         throw new ValorNaoSuportado(this.contentType)
     }
+
+    filtraObjeto (dados) {
+        const novoObjeto = {}
+        
+        this.camposPublicos.forEach((campo) => {
+            if(dados.hasOwnProperty(campo)) {
+                novoObjeto[campo] = dados[campo]
+            }
+        })
+
+        return novoObjeto
+    }
+
+    filtrar (dados) {
+        if (Array.isArray(dados)) {
+            dados = dados.map(item => {
+                return this.filtraObjeto(item)
+            })
+            
+        } else {
+            dados = this.filtraObjeto(dados)
+        }
+
+        return dados
+    }
+}
+
+class SerializadorFornecedor extends Serializador {
+    constructor (contentType) {
+        super()
+        this.contentType = contentType
+        this.camposPublicos = [
+            'id',
+            'empresa',
+            'categoria'
+        ]
+    }
+}
+
+module.exports = {
+    Serializador: Serializador,
+    SerializadorFornecedor: SerializadorFornecedor,
+    formatosAceitos: ['application/json']
 }
